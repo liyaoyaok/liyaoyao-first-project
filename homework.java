851442edb9bc30986987b082c2d799cdaa773504
李瑶瑶 <2157964938@qq.com>
2026-04-09 15:09:45 +0800

    Create homework.java
    计算1-10的和

---
+++ homework.java
@@ -0,0 +1,10 @@
+public class homework1 {
+    public static void main(String[] args) {
+        // 修改后的功能：计算1到10的和
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("1到10的累加和为：" + sum);
+    }
+}
