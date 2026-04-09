0cc718bcd42be06fb95792f8d83f5ceffd77238b
李瑶瑶 <2157964938@qq.com>
2026-04-09 15:08:22 +0800

    Delete homework1.java

--- homework1.java
+++
@@ -1,7 +0,0 @@
-public class homework1 {
-    public static void main(String[] args) {
-        // 这是修改后的内容
-        System.out.println("Hello, GitHub! 这是我修改后的第一个Java文件");
-        System.out.println("文件名：homework1.java");
-    }
-}
